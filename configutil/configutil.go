e606cafbee76feaca98d15e73adbea7d03ef35f7
Wonboyn <>
2020-08-26 17:41:48 +1000

    feature/SS-10 Extended configutils for file & env vars

--- configutil/configutil.go
+++ configutil/configutil.go
@@ -2,14 +2,31 @@ package configutil
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/spf13/viper"
 )
+
+// ConfigFile structure represents the metadata for a
+// configuration file that Viper can load
+type ConfigFile struct {
+	Name string
+	Path string
+	Type string
+}
 
 // DefaultValue represents a default configuration value
 type DefaultValue struct {
 	ConfigKey   string
 	ConfigValue string
+}
+
+// EnvVariable represents an environment variable and
+// the target configuration key to place the value
+type EnvVariable struct {
+	EnvVar    string
+	ConfigKey string
 }
 
 func loadDefaults(config *viper.Viper, defaults []DefaultValue) error {
@@ -34,12 +51,89 @@ func loadDefaults(config *viper.Viper, defaults []DefaultValue) error {
 //     svc := configutil.NewConfig(defaults)
 func NewConfig(defaults []DefaultValue) (*viper.Viper, error) {
 
+	// Instantiate Viper
 	vCfg := viper.New()
 
 	// Handle any default values
 	var err error = nil
 	if len(defaults) > 0 {
 		err = loadDefaults(vCfg, defaults)
+	}
+
+	// Return the configuration object
+	return vCfg, err
+}
+
+// NewConfigFile creates a new configuration file instance
+// that can then be used to instantiate a configuration manager.
+//
+// Example:
+//
+//     // Create a config file instance
+//     svc := configutil.NewConfigFile(fileName, fileType, filePath)
+func NewConfigFile(fileName string, fileType string, filePath string) (ConfigFile, error) {
+
+	// Sanity checks
+	var conf ConfigFile
+	var err error = nil
+	if fileName == "" {
+		err = errors.New("File name must be provided")
+		return conf, err
+	}
+	if fileType == "" {
+		err = errors.New("File type must be provided")
+		return conf, err
+	}
+	switch strings.ToUpper(fileType) {
+	case "JSON", "TOML", "YAML":
+		// These are ok....
+	default:
+		err = fmt.Errorf("Unsupported configuration file type: %s", fileType)
+		return conf, err
+	}
+
+	// Return the configuration object
+	conf.Name = fileName
+	conf.Path = filePath
+	conf.Type = fileType
+	return conf, err
+}
+
+// NewConfigFromEnv creates a new instance of the
+// configuration manager using environment variable(s)
+//
+// Example:
+//
+//     // Create an empty config manager instance
+//     svc := configutil.NewConfigFromEnv(defaults, envVars)
+func NewConfigFromEnv(defaults []DefaultValue, envVars []EnvVariable) (*viper.Viper, error) {
+
+	// Instantiate Viper
+	vCfg := viper.New()
+
+	// Handle any default values
+	var err error = nil
+	if len(defaults) > 0 {
+		err = loadDefaults(vCfg, defaults)
+	}
+
+	// Try & load the environment variables
+	for _, n := range envVars {
+
+		// Sanity check
+		key := n.ConfigKey
+		env := n.EnvVar
+		if env == "" {
+			err := errors.New("Empty environment variable provided")
+			return nil, err
+		}
+
+		// Ok bind the environment variable
+		if key == "" {
+			vCfg.BindEnv(env)
+		} else {
+			vCfg.BindEnv(key, env)
+		}
 	}
 
 	// Return the configuration object
@@ -53,8 +147,9 @@ func NewConfig(defaults []DefaultValue) (*viper.Viper, error) {
 //
 //     // Create an empty config manager instance
 //     svc := configutil.NewConfigFromFile(defaults, configfile)
-func NewConfigFromFile(defaults []DefaultValue) (*viper.Viper, error) {
+func NewConfigFromFile(defaults []DefaultValue, configfile ConfigFile) (*viper.Viper, error) {
 
+	// Instantiate Viper
 	vCfg := viper.New()
 
 	// Handle any default values
@@ -62,6 +157,12 @@ func NewConfigFromFile(defaults []DefaultValue) (*viper.Viper, error) {
 	if len(defaults) > 0 {
 		err = loadDefaults(vCfg, defaults)
 	}
+
+	// Try & load the config file
+	vCfg.SetConfigName(configfile.Name)
+	vCfg.SetConfigType(configfile.Type)
+	vCfg.AddConfigPath(configfile.Path)
+	err = vCfg.ReadInConfig()
 
 	// Return the configuration object
 	return vCfg, err
--- configutil/configutil_test.go
+++ configutil/configutil_test.go
@@ -60,3 +60,178 @@ func TestNewConfig(t *testing.T) {
 		}
 	}
 }
+
+// Test NewConfigFile
+func TestNewConfigFile(t *testing.T) {
+
+	// Setup test data
+	var tests = []struct {
+		name        string
+		fileName    string
+		fileType    string
+		filePath    string
+		expectErr   bool
+		expectedVal string
+	}{
+		{"No values", "", "", "", true, ""},
+		{"Just fileName", "fred", "", "", true, ""},
+		{"With fileName & filePath", "fred", "", "fred", true, ""},
+		{"With all - invalid fileType", "fred", "fred", "fred", true, ""},
+		{"With all - valid fileType", "fred", "json", "fred", false, ""},
+	}
+
+	// Iterate through the test data
+	assert := assert.New(t)
+	for _, test := range tests {
+
+		// Run each test
+		_, err := configutil.NewConfigFile(test.fileName, test.fileType, test.filePath)
+		if test.expectErr {
+			assert.NotNil(err, test.name)
+		} else {
+			assert.Nil(err, test.name)
+		}
+	}
+}
+
+// Test NewConfigFromEnv
+func NewConfigFromEnv(t *testing.T) {
+
+	// Setup test default values
+	validDefault := []configutil.DefaultValue{
+		{
+			ConfigKey:   "configKey",
+			ConfigValue: "configVal",
+		},
+		{
+			ConfigKey:   "shell",
+			ConfigValue: "default",
+		}}
+	noKeyDefault := []configutil.DefaultValue{{
+		ConfigValue: "configVal",
+	}}
+	emptyKeyDefault := []configutil.DefaultValue{{
+		ConfigKey:   "",
+		ConfigValue: "configVal",
+	}}
+	noValueDefault := []configutil.DefaultValue{{
+		ConfigKey: "configKey",
+	}}
+	emptyValueDefault := []configutil.DefaultValue{{
+		ConfigKey:   "configKey",
+		ConfigValue: "",
+	}}
+
+	// Setup test environment variable values
+	var emptyConfFile configutil.EnvVariable
+	validConfFile, _ := configutil.NewConfigFile("testdata", "yaml", ".")
+	missingConfFile, _ := configutil.NewConfigFile("testdata2", "yaml", ".")
+	invalidConfFileType, _ := configutil.NewConfigFile("testdata", "json", ".")
+
+	// Setup test data
+	var tests = []struct {
+		name             string
+		defaults         []configutil.DefaultValue
+		envVars          []configutil.EnvVariable
+		expectErr        bool
+		configKeyToCheck string
+		expectedVal      string
+	}{
+		{"No values", nil, nil, false, "", ""},
+		{"Default config with no key", noKeyDefault, emptyConfFile, true, "", ""},
+		{"Default config with empty key", emptyKeyDefault, emptyConfFile, true, "", ""},
+		{"Default config with no value", noValueDefault, validConfFile, false, "configKey", ""},
+		{"Default config with empty value", emptyValueDefault, validConfFile, false, "configKey", ""},
+		{"Valid default config with empty config file", validDefault, emptyConfFile, true, "", ""},
+		{"Valid default config with missing config file", validDefault, missingConfFile, true, "", ""},
+		{"Valid default config with invalid config file type", validDefault, invalidConfFileType, true, "", ""},
+		{"Valid call", validDefault, validConfFile, false, "configKey", "configVal"},
+		{"Valid call", validDefault, validConfFile, false, "metadata.labels.test", "not-default"},
+	}
+
+	// Iterate through the test data
+	assert := assert.New(t)
+	for _, test := range tests {
+
+		// Run each test
+		obj, err := configutil.NewConfigFromFile(test.defaults, test.confFile)
+		if test.expectErr {
+			assert.NotNil(err, test.name)
+		} else {
+			actualVal := obj.GetString(test.configKeyToCheck)
+			assert.Nil(err, test.name)
+			assert.Equal(test.expectedVal, actualVal, test.name)
+		}
+	}
+}
+
+// Test NewConfigFromFile
+func TestNewConfigFromFile(t *testing.T) {
+
+	// Setup test default values
+	validDefault := []configutil.DefaultValue{
+		{
+			ConfigKey:   "configKey",
+			ConfigValue: "configVal",
+		},
+		{
+			ConfigKey:   "metadata.labels.test",
+			ConfigValue: "default",
+		}}
+	noKeyDefault := []configutil.DefaultValue{{
+		ConfigValue: "configVal",
+	}}
+	emptyKeyDefault := []configutil.DefaultValue{{
+		ConfigKey:   "",
+		ConfigValue: "configVal",
+	}}
+	noValueDefault := []configutil.DefaultValue{{
+		ConfigKey: "configKey",
+	}}
+	emptyValueDefault := []configutil.DefaultValue{{
+		ConfigKey:   "configKey",
+		ConfigValue: "",
+	}}
+
+	// Setup test config file values
+	var emptyConfFile configutil.ConfigFile
+	validConfFile, _ := configutil.NewConfigFile("testdata", "yaml", ".")
+	missingConfFile, _ := configutil.NewConfigFile("testdata2", "yaml", ".")
+	invalidConfFileType, _ := configutil.NewConfigFile("testdata", "json", ".")
+
+	// Setup test data
+	var tests = []struct {
+		name             string
+		defaults         []configutil.DefaultValue
+		confFile         configutil.ConfigFile
+		expectErr        bool
+		configKeyToCheck string
+		expectedVal      string
+	}{
+		{"No values", nil, emptyConfFile, true, "", ""},
+		{"Default config with no key", noKeyDefault, emptyConfFile, true, "", ""},
+		{"Default config with empty key", emptyKeyDefault, emptyConfFile, true, "", ""},
+		{"Default config with no value", noValueDefault, validConfFile, false, "configKey", ""},
+		{"Default config with empty value", emptyValueDefault, validConfFile, false, "configKey", ""},
+		{"Valid default config with empty config file", validDefault, emptyConfFile, true, "", ""},
+		{"Valid default config with missing config file", validDefault, missingConfFile, true, "", ""},
+		{"Valid default config with invalid config file type", validDefault, invalidConfFileType, true, "", ""},
+		{"Valid call", validDefault, validConfFile, false, "configKey", "configVal"},
+		{"Valid call", validDefault, validConfFile, false, "metadata.labels.test", "not-default"},
+	}
+
+	// Iterate through the test data
+	assert := assert.New(t)
+	for _, test := range tests {
+
+		// Run each test
+		obj, err := configutil.NewConfigFromFile(test.defaults, test.confFile)
+		if test.expectErr {
+			assert.NotNil(err, test.name)
+		} else {
+			actualVal := obj.GetString(test.configKeyToCheck)
+			assert.Nil(err, test.name)
+			assert.Equal(test.expectedVal, actualVal, test.name)
+		}
+	}
+}
