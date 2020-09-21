ccc03a96b611879fc03a355f1a97199622704ba6
Wonboyn <>
2020-09-21 11:43:20 +1000

    feature/SM Part of fix for bradmccoydev/self-service-sdk#5

--- aws/secretsmanager/secretsmanager_test.go
+++ aws/secretsmanager/secretsmanager_test.go
@@ -34,6 +34,43 @@ func TestMain(m *testing.M) {
 	os.Exit(exitVal)
 }
 
+// Test GetSecretKeyValue
+func TestGetSecretKeyValue(t *testing.T) {
+
+	// Setup test data
+	tests := []struct {
+		desc       string
+		validSess  bool
+		secretName string
+		expectErr  bool
+	}{
+		{"No session", false, "", true},
+		{"With session but no secret name", true, "", true},
+		{"With session and invalid secret name", true, "Fred", false},
+	}
+
+	// Iterate through the test data
+	for _, test := range tests {
+
+		t.Run(test.desc, func(t *testing.T) {
+
+			// Run the test
+			var sess *session.Session
+			if test.validSess {
+				sess = internal.CreateAwsSession(true)
+			} else {
+				sess = internal.CreateAwsSession(false)
+			}
+			_, err := secretsmanager.GetSecretKeyValue(sess, test.secretName)
+			if test.expectErr {
+				internal.HasError(t, err)
+			} else {
+				internal.NoError(t, err)
+			}
+		})
+	}
+}
+
 // Test GetSecretString
 func TestGetSecretString(t *testing.T) {
 
